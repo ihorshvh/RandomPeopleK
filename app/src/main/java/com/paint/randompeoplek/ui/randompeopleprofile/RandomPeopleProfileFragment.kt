e3e338e05172b577f185eb0e8876e1ccd36e960d
user <shvedenenkoio@gmail.com>
2021-04-27 10:02:31 -0700

    Add resources

--- app/src/main/java/com/paint/randompeoplek/ui/randompeopleprofile/RandomPeopleProfileFragment.kt
+++ app/src/main/java/com/paint/randompeoplek/ui/randompeopleprofile/RandomPeopleProfileFragment.kt
@@ -25,6 +25,7 @@ class RandomPeopleProfileFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
         viewModel = ViewModelProvider(this).get(RandomPeopleProfileViewModel::class.java)
         // TODO: Use the ViewModel
     }
