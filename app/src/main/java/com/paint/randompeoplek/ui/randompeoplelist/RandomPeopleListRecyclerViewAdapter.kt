ec52ab903ccf5f7b94f8ca9110d5d60b3f0bb378
user <shvedenenkoio@gmail.com>
2021-05-06 11:23:03 -0700

    Added view binding to adapter

--- app/src/main/java/com/paint/randompeoplek/ui/randompeoplelist/RandomPeopleListRecyclerViewAdapter.kt
+++ app/src/main/java/com/paint/randompeoplek/ui/randompeoplelist/RandomPeopleListRecyclerViewAdapter.kt
@@ -2,10 +2,8 @@ package com.paint.randompeoplek.ui.randompeoplelist
 
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import com.paint.randompeoplek.R
+import com.paint.randompeoplek.databinding.RandomPeopleListItemBinding
 
 import com.paint.randompeoplek.ui.randompeoplelist.dummy.DummyContent.DummyItem
 
@@ -18,25 +16,20 @@ class RandomPeopleListRecyclerViewAdapter(
 ) : RecyclerView.Adapter<RandomPeopleListRecyclerViewAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.random_people_list_item, parent, false)
-        return ViewHolder(view)
+        val binding = RandomPeopleListItemBinding
+                .inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = values[position]
-        holder.idView.text = item.id
-        holder.contentView.text = item.content
+        holder.binding.tvUserName.text = item.id
+        holder.binding.tvAddress.text = item.content
     }
 
     override fun getItemCount(): Int = values.size
 
-    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val idView: TextView = view.findViewById(R.id.tv_user_name)
-        val contentView: TextView = view.findViewById(R.id.tv_address)
-
-        override fun toString(): String {
-            return super.toString() + " '" + contentView.text + "'"
-        }
-    }
+    inner class ViewHolder(val binding : RandomPeopleListItemBinding)
+        : RecyclerView.ViewHolder(binding.root)
 }
