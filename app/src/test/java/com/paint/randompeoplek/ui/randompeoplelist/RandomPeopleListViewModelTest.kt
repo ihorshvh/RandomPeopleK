9af130819e46bfc85383ea5619c872a92f4d5bcd
ihorshvedenenko <shvedenenkoio@gmail.com>
2024-06-16 14:12:30 -0700

    fixed working of random people view model list test class

--- app/src/test/java/com/paint/randompeoplek/ui/randompeoplelist/RandomPeopleListViewModelTest.kt
+++ app/src/test/java/com/paint/randompeoplek/ui/randompeoplelist/RandomPeopleListViewModelTest.kt
@@ -1,48 +1,37 @@
 package com.paint.randompeoplek.ui.randompeoplelist
 
-import app.cash.turbine.test
+import app.cash.turbine.turbineScope
 import com.paint.randompeoplek.domain.RandomPeopleListUseCase
 import com.paint.randompeoplek.domain.errorhandler.ErrorEntity
-import com.paint.randompeoplek.domain.errorhandler.ErrorHandlerUseCase
 import com.paint.randompeoplek.domain.errorhandler.ErrorHandlerUseCaseImpl
 import com.paint.randompeoplek.domain.model.UserResponse
 import com.paint.randompeoplek.model.Response
-import com.paint.randompeoplek.ui.model.User
-import com.paint.randompeoplek.ui.model.toUiParcelableUsers
 import io.mockk.coEvery
-import io.mockk.every
 import io.mockk.mockk
-import junit.framework.Assert.assertNull
 import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertFalse
+import junit.framework.TestCase.assertTrue
 import junit.framework.TestCase.assertNotNull
+import junit.framework.TestCase.assertNull
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.toList
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
-import org.hamcrest.CoreMatchers
 import org.hamcrest.CoreMatchers.instanceOf
-import org.hamcrest.MatcherAssert
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
-import retrofit2.HttpException
-import java.io.IOException
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class RandomPeopleListViewModelTest {
 
 //    @get:Rule
 //    val coroutineRule = CoroutineRule()
-//
-//    @get:Rule
-//    val instantExecutorRule = InstantTaskExecutorRule()
 
     @Test
     fun testGetUserListWhenSuccess() = runTest {
-        val testDispatcher = UnconfinedTestDispatcher(testScheduler)
+        val testDispatcher = StandardTestDispatcher(testScheduler)
         Dispatchers.setMain(testDispatcher)
 
         try {
@@ -55,31 +44,30 @@ class RandomPeopleListViewModelTest {
 
             val viewModel = RandomPeopleListViewModel(randomPeopleListMediator, ErrorHandlerUseCaseImpl())
 
-            val usersResponseStates = mutableListOf<Response<List<User>>>()
-            backgroundScope.launch(UnconfinedTestDispatcher(testScheduler)) {
-                viewModel.usersResponseFlow.toList(usersResponseStates)
+            turbineScope {
+                val usersResponseFlows = viewModel.usersResponseFlow.testIn(backgroundScope)
+                val isRefreshingFlows = viewModel.isRefreshing.testIn(backgroundScope)
+                val oneTimeErrorFlows = viewModel.oneTimeErrorFlow.testIn(backgroundScope)
+
+                assertThat(usersResponseFlows.awaitItem(), instanceOf(Response.Initial::class.java))
+
+                val userResponse = usersResponseFlows.awaitItem()
+                assertThat(userResponse, instanceOf(Response.Success::class.java))
+                assertEquals(2, userResponse.data?.size)
+                assertNull(userResponse.errorEntity)
+                assertFalse(isRefreshingFlows.awaitItem())
+
+                viewModel.getRandomPeopleList("10")
+
+                assertTrue(isRefreshingFlows.awaitItem())
+
+                val userResponseRefreshed = usersResponseFlows.awaitItem()
+                assertThat(userResponseRefreshed, instanceOf(Response.Success::class.java))
+                assertEquals(2, userResponseRefreshed.data?.size)
+                assertNull(userResponseRefreshed.errorEntity)
+
+                assertFalse(isRefreshingFlows.awaitItem())
             }
-
-            val isRefreshingStates = mutableListOf<Boolean>()
-            backgroundScope.launch(UnconfinedTestDispatcher(testScheduler)) {
-                viewModel.isRefreshing.toList(isRefreshingStates)
-            }
-
-            val onTimeErrorStates = mutableListOf<ErrorEntity>()
-            backgroundScope.launch(UnconfinedTestDispatcher(testScheduler)) {
-                viewModel.oneTimeErrorFlow.toList(onTimeErrorStates)
-            }
-
-            assertEquals(1, usersResponseStates.size)
-            val state = usersResponseStates[0]
-            assertThat(state, instanceOf(Response.Success::class.java))
-            assertEquals(2, state.data?.size)
-            assertNull(state.errorEntity)
-
-            assertEquals(1, isRefreshingStates.size)
-            assertEquals(isRefreshingStates[0], false)
-
-            assertEquals(0, onTimeErrorStates.size)
         } finally {
             Dispatchers.resetMain()
         }
@@ -87,39 +75,45 @@ class RandomPeopleListViewModelTest {
 
     @Test
     fun testGetRandomPeopleListWhenSuccessButWithError() = runTest {
-        val testDispatcher = UnconfinedTestDispatcher(testScheduler)
+        val testDispatcher = StandardTestDispatcher(testScheduler)
         Dispatchers.setMain(testDispatcher)
 
         try {
-            val exception = Throwable("exception")
-
             val randomPeopleListMediator = mockk<RandomPeopleListUseCase>()
             val users = getUsers()
+
+            val exception = Throwable("exception")
             coEvery { randomPeopleListMediator.getUserList("10") } answers {
                 UserResponse(users, exception)
             }
 
             val viewModel = RandomPeopleListViewModel(randomPeopleListMediator, ErrorHandlerUseCaseImpl())
 
-            val usersResponseStates = mutableListOf<Response<List<User>>>()
-            backgroundScope.launch(UnconfinedTestDispatcher(testScheduler)) {
-                viewModel.usersResponseFlow.toList(usersResponseStates)
+            turbineScope {
+                val usersResponseFlows = viewModel.usersResponseFlow.testIn(backgroundScope)
+                val isRefreshingFlows = viewModel.isRefreshing.testIn(backgroundScope)
+                val oneTimeErrorFlows = viewModel.oneTimeErrorFlow.testIn(backgroundScope)
+
+                assertThat(usersResponseFlows.awaitItem(), instanceOf(Response.Initial::class.java))
+
+                val userResponse = usersResponseFlows.awaitItem()
+                assertThat(userResponse, instanceOf(Response.Error::class.java))
+                assertEquals(2, userResponse.data?.size)
+                assertNotNull(userResponse.errorEntity)
+                assertFalse(isRefreshingFlows.awaitItem())
+                assertThat(oneTimeErrorFlows.awaitItem(), instanceOf(ErrorEntity.Unknown::class.java))
+
+                viewModel.getRandomPeopleList("10")
+
+                assertTrue(isRefreshingFlows.awaitItem())
+
+                val userResponseRefreshed = usersResponseFlows.awaitItem()
+                assertThat(userResponseRefreshed, instanceOf(Response.Error::class.java))
+                assertEquals(2, userResponseRefreshed.data?.size)
+                assertNotNull(userResponseRefreshed.errorEntity)
+                assertFalse(isRefreshingFlows.awaitItem())
+                assertThat(oneTimeErrorFlows.awaitItem(), instanceOf(ErrorEntity.Unknown::class.java))
             }
-
-            assertEquals(1, usersResponseStates.size)
-            val state = usersResponseStates[0]
-            assertThat(state, instanceOf(Response.Error::class.java))
-            assertEquals(2, state.data?.size)
-            assertNotNull(state.errorEntity)
-
-            backgroundScope.launch(UnconfinedTestDispatcher(testScheduler)) {
-                viewModel.oneTimeErrorFlow.test {
-                    val errorEntity = awaitItem()
-                    assertEquals(ErrorEntity.Unknown, errorEntity)
-                }
-            }
-
-            viewModel.getRandomPeopleList("10")
         } finally {
             Dispatchers.resetMain()
         }
@@ -127,7 +121,7 @@ class RandomPeopleListViewModelTest {
 
     @Test
     fun testGetRandomPeopleListWhenFailure() = runTest {
-        val testDispatcher = UnconfinedTestDispatcher(testScheduler)
+        val testDispatcher = StandardTestDispatcher(testScheduler)
         Dispatchers.setMain(testDispatcher)
 
         try {
@@ -138,27 +132,21 @@ class RandomPeopleListViewModelTest {
             }
 
             val viewModel = RandomPeopleListViewModel(randomPeopleListMediator, ErrorHandlerUseCaseImpl())
-            val usersResponseStates = mutableListOf<Response<List<User>>>()
-            backgroundScope.launch(UnconfinedTestDispatcher(testScheduler)) {
-                viewModel.usersResponseFlow.toList(usersResponseStates)
+
+            turbineScope {
+                val usersResponseFlows = viewModel.usersResponseFlow.testIn(backgroundScope)
+                val isRefreshingFlows = viewModel.isRefreshing.testIn(backgroundScope)
+                val oneTimeErrorFlows = viewModel.oneTimeErrorFlow.testIn(backgroundScope)
+
+                assertThat(usersResponseFlows.awaitItem(), instanceOf(Response.Initial::class.java))
+                assertThat(oneTimeErrorFlows.awaitItem(), instanceOf(ErrorEntity.Unknown::class.java))
+
+                viewModel.getRandomPeopleList("10")
+                assertThat(oneTimeErrorFlows.awaitItem(), instanceOf(ErrorEntity.Unknown::class.java))
+                assertFalse(isRefreshingFlows.awaitItem())
             }
-
-            viewModel.getRandomPeopleList("10")
-
-            assertEquals(1, usersResponseStates.size)
-            assertThat(usersResponseStates[0], instanceOf(Response.Initial::class.java))
-
-            backgroundScope.launch(UnconfinedTestDispatcher(testScheduler)) {
-                viewModel.oneTimeErrorFlow.test {
-                    val errorEntity = awaitItem()
-                    assertEquals(ErrorEntity.Unknown, errorEntity)
-                }
-            }
-
-            viewModel.getRandomPeopleList("10")
         } finally {
             Dispatchers.resetMain()
         }
     }
-
 }
