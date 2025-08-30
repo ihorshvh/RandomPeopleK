94900164ea323cd3b1f3ecc5df5e195493ed528d
Ihor Shvedenenko <ihorshvedenenko@Mac.lan>
2025-08-29 20:34:52 -0700

    migrated to toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,24 +1,23 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.kapt")
-    id("org.jetbrains.kotlin.plugin.parcelize")
-    id("com.google.dagger.hilt.android")
-    id("org.jetbrains.kotlin.plugin.compose")
-    id("com.google.devtools.ksp")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
 }
 
 android {
-    namespace = "com.paint.randompeoplek"
-    compileSdk = rootProject.extra["compile_sdk_version"] as Int
+    namespace = libs.versions.applicationId.get()
+    compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
-        applicationId = rootProject.extra["application_id"] as String
-        minSdk = rootProject.extra["min_sdk_version"] as Int
-        targetSdk = rootProject.extra["target_sdk_version"] as Int
-        versionCode = rootProject.extra["version_code"] as Int
-        versionName = rootProject.extra["version_name"] as String
-        testInstrumentationRunner = rootProject.extra["test_instrumentation_runner"] as String
+        applicationId = libs.versions.applicationId.get()
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+        versionCode = libs.versions.versionCode.get().toInt()
+        versionName = libs.versions.versionName.get()
+        testInstrumentationRunner = libs.versions.testInstrumentationRunner.get()
 
         javaCompileOptions {
             annotationProcessorOptions {
@@ -40,7 +39,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.11" // Consider moving this to rootProject.extra
+        kotlinCompilerExtensionVersion = libs.versions.composeCompilerExtension.get()
     }
 
     compileOptions {
@@ -49,71 +48,68 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = rootProject.extra["jvm_target"] as String
+        jvmTarget = libs.versions.jvmTarget.get()
     }
 }
 
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:${rootProject.extra["kotlin_version"]}")
+    implementation(libs.kotlin.stdlib)
 
-    implementation("androidx.appcompat:appcompat:${rootProject.extra["appcompat_version"]}")
-    implementation("androidx.core:core-ktx:${rootProject.extra["core_ktx_version"]}")
-    implementation("androidx.core:core-splashscreen:${rootProject.extra["splash_screen_version"]}")
-    implementation("androidx.constraintlayout:constraintlayout:${rootProject.extra["constraintlayout_version"]}")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:${rootProject.extra["lifecycle_version"]}")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:${rootProject.extra["lifecycle_version"]}")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:${rootProject.extra["lifecycle_version"]}")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:${rootProject.extra["lifecycle_version"]}")
-    implementation("androidx.legacy:legacy-support-v4:${rootProject.extra["legacy_support_version"]}")
-    implementation("androidx.recyclerview:recyclerview:${rootProject.extra["recyclerview_version"]}")
-    implementation("androidx.coordinatorlayout:coordinatorlayout:${rootProject.extra["coordinatorlayout_version"]}")
-    implementation("androidx.cardview:cardview:${rootProject.extra["cardview_version"]}")
-    implementation("androidx.fragment:fragment-ktx:${rootProject.extra["fragment_ktx_version"]}")
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.core.splashscreen)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.lifecycle.livedata.ktx) 
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.legacy.support.v4)
+    implementation(libs.androidx.recyclerview)
+    implementation(libs.androidx.coordinatorlayout)
+    implementation(libs.androidx.cardview)
+    implementation(libs.androidx.fragment.ktx)
 
-    implementation("com.squareup.retrofit2:retrofit:${rootProject.extra["retrofit_version"]}")
-    implementation("com.squareup.retrofit2:converter-gson:${rootProject.extra["retrofit_version"]}")
+    implementation(libs.retrofit.core) // Corrected line
+    implementation(libs.retrofit.converter.gson)
 
-    implementation("com.google.dagger:hilt-android:${rootProject.extra["hilt_version"]}")
-    implementation("com.google.android.material:material:${rootProject.extra["material"]}")
-    kapt("com.google.dagger:hilt-compiler:${rootProject.extra["hilt_version"]}")
+    implementation(libs.hilt.android)
+    implementation(libs.material.components)
+    ksp(libs.hilt.compiler)
 
-    implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
-    implementation("androidx.room:room-ktx:${rootProject.extra["room_version"]}")
-    ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
-    testImplementation("androidx.room:room-testing:${rootProject.extra["room_version"]}")
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
+    testImplementation(libs.androidx.arch.core.testing)
 
     // navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:${rootProject.extra["nav_version"]}")
-    implementation("androidx.navigation:navigation-ui-ktx:${rootProject.extra["nav_version"]}")
-    implementation("androidx.navigation:navigation-dynamic-features-fragment:${rootProject.extra["nav_version"]}")
-    implementation("androidx.navigation:navigation-compose:${rootProject.extra["nav_version"]}")
-    implementation("androidx.hilt:hilt-navigation-compose:${rootProject.extra["hilt_nav_version"]}")
-    androidTestImplementation("androidx.navigation:navigation-testing:${rootProject.extra["nav_version"]}")
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.navigation.dynamic.features.fragment)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.hilt.navigation.compose)
+    androidTestImplementation(libs.androidx.navigation.testing)
 
     // compose
-    val composeBom = platform("androidx.compose:compose-bom:2025.08.00")
+    val composeBom = platform(libs.androidx.compose.bom)
     implementation(composeBom)
     androidTestImplementation(composeBom)
-    implementation("androidx.compose.material:material:${rootProject.extra["compose_material"]}")
-    implementation("androidx.activity:activity-compose:${rootProject.extra["compose_activity"]}")
+    implementation(libs.androidx.compose.material)
+    implementation(libs.androidx.compose.activity)
     // Android Studio Preview support
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    implementation("io.coil-kt:coil-compose:2.7.0")
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    implementation(libs.coil.compose)
     // compose
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:${rootProject.extra["kotlinx_coroutines_version"]}")
+    implementation(libs.kotlinx.coroutines.android)
 
-    testImplementation("junit:junit:${rootProject.extra["junit_version"]}")
+    testImplementation(libs.junit)
+    testImplementation(libs.mockk.android)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.turbine)
 
-    testImplementation("io.mockk:mockk:${rootProject.extra["mockK"]}")
-
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:${rootProject.extra["coroutines_test"]}")
-    testImplementation("androidx.arch.core:core-testing:${rootProject.extra["androidx_core_testing"]}")
-    testImplementation("app.cash.turbine:turbine:${rootProject.extra["turbine"]}")
-
-    androidTestImplementation("androidx.test:runner:${rootProject.extra["test_runner_version"]}")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test:core:1.4.0")
+    androidTestImplementation(libs.androidx.test.runner)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.androidx.test.core)
 }
