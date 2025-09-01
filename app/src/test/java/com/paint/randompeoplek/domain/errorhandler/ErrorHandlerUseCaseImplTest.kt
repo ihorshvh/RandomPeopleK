92d282cd60e5061dbdcba96e280afaa926cd72f6
Ihor Shvedenenko <ihorshvedenenko@Mac.lan>
2025-09-01 15:23:11 -0700

    upgraded ErrorHandlerUseCaseImplTest.kt

--- app/src/test/java/com/paint/randompeoplek/domain/errorhandler/ErrorHandlerUseCaseImplTest.kt
+++ app/src/test/java/com/paint/randompeoplek/domain/errorhandler/ErrorHandlerUseCaseImplTest.kt
@@ -1,43 +1,45 @@
 package com.paint.randompeoplek.domain.errorhandler
 
+import com.paint.randompeoplek.repository.NetworkError
+import com.paint.randompeoplek.resourceprovider.ResourceProvider
+import io.mockk.mockk
 import io.mockk.*
-import org.junit.Test
-import org.junit.Assert.*
-import retrofit2.HttpException
-import retrofit2.Response
+import org.junit.jupiter.api.Assertions.assertEquals
 import java.io.IOException
-import java.lang.ArithmeticException
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class ErrorHandlerUseCaseImplTest {
-// TODO fix tests
-//    private val errorHandlerUseCase : ErrorHandlerUseCase = ErrorHandlerUseCaseImpl()
-//
-//    @Test
-//    fun testGetErrorMessageWhenIOException() {
-//        val errorEntity = errorHandlerUseCase.getErrorMessage(IOException())
-//        //assertEquals(ErrorEntity.Network, errorEntity)
-//    }
-//
-//    @Test
-//    fun testGetErrorMessageWhenHttpException() {
-//        val response = mockkClass(Response::class)
-//
-//        every {
-//            response.code()
-//        } returns 400
-//
-//        every {
-//            response.message()
-//        } returns "Service Unavailable"
-//
-//        val errorEntity = errorHandlerUseCase.getErrorMessage(HttpException(response))
-//        assertEquals(ErrorEntity.ServiceUnavailable, errorEntity)
-//    }
-//
-//    @Test
-//    fun testGetErrorMessageWhenOtherException() {
-//        val errorEntity = errorHandlerUseCase.getErrorMessage(ArithmeticException())
-//        assertEquals(ErrorEntity.Unknown, errorEntity)
-//    }
 
+    private lateinit var errorHandlerUseCase : ErrorHandlerUseCase
+    private lateinit var resourceProvider: ResourceProvider
+
+    @BeforeEach
+    fun setUp() {
+        resourceProvider = mockk()
+        errorHandlerUseCase = ErrorHandlerUseCaseImpl(resourceProvider)
+    }
+
+    @Test
+    fun testGetErrorMessageWhenException() {
+        every { resourceProvider.getString(any()) } returns "Unknown error"
+        val errorMessage = errorHandlerUseCase.getErrorMessage(Exception())
+        assertEquals("Unknown error", errorMessage)
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "REQUEST_TIMEOUT, Timeout error",
+        "UNAUTHORIZED, Unauthorized error",
+        "NO_INTERNET, No internet error",
+        "SERVER_ERROR, Server error",
+        "UNKNOWN, Unknown error"
+    )
+    fun testGetErrorMessageWhenNetworkError(networkError: NetworkError, expectedMessage: String) {
+        every { resourceProvider.getString(any()) } returns expectedMessage
+        val errorMessage = errorHandlerUseCase.getErrorMessage(networkError)
+        assertEquals(expectedMessage, errorMessage)
+    }
 }
