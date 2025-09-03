ff0fddffe175834b23b46d31d0ab5d6ce2b6f6c9
Ihor Shvedenenko <ihorshvedenenko@Mac.lan>
2025-09-02 17:48:39 -0700

    material_3_migration: cleanup theme logic

--- app/src/main/java/com/paint/randompeoplek/ui/theme/Color.kt
+++ app/src/main/java/com/paint/randompeoplek/ui/theme/Color.kt
@@ -7,11 +7,6 @@ val white800 = Color.White.copy(alpha = 0.8f)
 
 val lime500 = Color(0xFFCDDC39)
 val lime700 = Color(0xFFAFB42B)
-val lightGreen500 = Color(0xFF8BC34A)
-
-val white150 = Color.White.copy(alpha = 0.15f)
 val grey = Color(0x26999090)
-val greyTransparent = Color(0x98FFFFFF)
 
 val lime900 = Color(0xFF827717)
-val green900 = Color(0xFF1B5E20)
--- app/src/main/java/com/paint/randompeoplek/ui/theme/Theme.kt
+++ app/src/main/java/com/paint/randompeoplek/ui/theme/Theme.kt
@@ -1,15 +1,11 @@
 package com.paint.randompeoplek.ui.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 
 private val LightColorScheme = lightColorScheme(
     primary = lime500,
@@ -19,11 +15,7 @@ private val LightColorScheme = lightColorScheme(
     onPrimary = Color.White,
     onSecondary = Color.White,
     onBackground = white800,
-    onSurface = gray900.copy(alpha = 0.8f),
-    tertiary = lime500,
-    surfaceVariant = Color.White,
-    onSurfaceVariant = gray900,
-    outline = gray900
+    onSurface = gray900.copy(alpha = 0.8f)
 )
 
 private val DarkColorScheme = darkColorScheme(
@@ -34,26 +26,18 @@ private val DarkColorScheme = darkColorScheme(
     onPrimary = gray900,
     onSecondary = gray900,
     onBackground = white800,
-    onSurface = Color.White.copy(alpha = .8f),
-    tertiary = lime700,
-    surfaceVariant = gray900,
-    onSurfaceVariant = Color.White,
-    outline = gray900
+    onSurface = Color.White.copy(alpha = .8f)
 )
 
 @Composable
 fun RandomPeopleKTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    val colorScheme = if (darkTheme) {
+        DarkColorScheme
+    } else {
+        LightColorScheme
     }
 
     MaterialTheme(
