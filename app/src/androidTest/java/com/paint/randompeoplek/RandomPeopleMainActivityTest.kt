ac38187ef860204d922005d44b0531c847a194fc
IS <shvedenenkoihor@gmail.com>
2025-09-19 09:51:02 -0700

    code cleanup

--- app/src/androidTest/java/com/paint/randompeoplek/RandomPeopleMainActivityTest.kt
+++ app/src/androidTest/java/com/paint/randompeoplek/RandomPeopleMainActivityTest.kt
@@ -13,14 +13,6 @@ import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performScrollToNode
 import androidx.compose.ui.test.performTextReplacement
-import com.paint.randompeoplek.di.RandomPeopleServiceModule
-import com.paint.randompeoplek.domain.RandomPeopleListUseCase
-import com.paint.randompeoplek.domain.model.Name
-import com.paint.randompeoplek.domain.model.Picture
-import com.paint.randompeoplek.domain.model.User
-import com.paint.randompeoplek.service.RandomPeopleService
-import com.paint.randompeoplek.service.model.Id
-import com.paint.randompeoplek.service.model.UserResponse
 import com.paint.randompeoplek.ui.randompeoplelist.TEST_TAG_LOADING_INDICATOR
 import com.paint.randompeoplek.ui.randompeoplelist.TEST_TAG_RANDOM_PEOPLE_LIST_LAST_USER
 import com.paint.randompeoplek.ui.randompeoplelist.TEST_TAG_RANDOM_PEOPLE_LIST_USER
@@ -33,28 +25,13 @@ import com.paint.randompeoplek.ui.randompeopleprofile.TEST_TAG_USER_IMAGE
 import com.paint.randompeoplek.ui.randompeopleprofile.TEST_TAG_USER_LOCATION_ICON
 import com.paint.randompeoplek.ui.randompeopleprofile.TEST_TAG_USER_NAME_ICON
 import com.paint.randompeoplek.ui.randompeopleprofile.TEST_TAG_USER_PHONE_ICON
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.android.testing.BindValue
 import dagger.hilt.android.testing.HiltAndroidRule
 import org.junit.Test
 import dagger.hilt.android.testing.HiltAndroidTest
-import dagger.hilt.android.testing.UninstallModules
-import dagger.hilt.components.SingletonComponent
-import dagger.hilt.testing.TestInstallIn
-import io.mockk.coEvery
-import io.mockk.mockk
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
-import org.junit.After
-import org.junit.Before
 import org.junit.Rule
-import retrofit2.Response
-import javax.inject.Inject
-import javax.inject.Singleton
 
 @HiltAndroidTest
-@UninstallModules(RandomPeopleServiceModule::class)
 class RandomPeopleMainActivityTest {
 
     @get:Rule(order = 0)
@@ -63,94 +40,9 @@ class RandomPeopleMainActivityTest {
     @get:Rule(order = 1)
     val composeRule = createAndroidComposeRule<RandomPeopleMainActivity>()
 
-//    @Inject
-//    lateinit var randomPeopleService: RandomPeopleService
-
-    @BindValue
-    @JvmField // JvmField is necessary for Hilt to correctly process the @BindValue field
-    val randomPeopleService: RandomPeopleService = object : RandomPeopleService {
-        override suspend fun getUserList(userQuantity: String): Response<com.paint.randompeoplek.service.model.UserResponse> {
-            return Response.success(
-                200,
-                com.paint.randompeoplek.service.model.UserResponse(
-                    listOf(
-                        com.paint.randompeoplek.service.model.User(
-                            id = Id("test_id"),
-                            name = com.paint.randompeoplek.service.model.Name("Mr", "Ryan", "Wilson"),
-                            location = com.paint.randompeoplek.service.model.Location(
-                                street = com.paint.randompeoplek.service.model.Street(740, "Lambie Drive"),
-                                city = "Invercargill",
-                                state = "Bay of Plenty",
-                                country = "New Zealand",
-                                postCode = "32336"
-                            ),
-                            email = "william.henry.harrison@example-pet-store.com",
-                            phone = "123-456-7890",
-                            picture = com.paint.randompeoplek.service.model.Picture(
-                                large = "https://randomuser.me/api/portraits/men/1.jpg",
-                                medium = "https://randomuser.me/api/portraits/med/men/1.jpg",
-                                thumbnail = "https://randomuser.me/api/portraits/thumb/men/1.jpg"
-                            )
-                        ),
-                        com.paint.randompeoplek.service.model.User(
-                            id = Id("test_id_2"),
-                            name = com.paint.randompeoplek.service.model.Name("Mr", "Test", "Name"),
-                            location = com.paint.randompeoplek.service.model.Location(
-                                street = com.paint.randompeoplek.service.model.Street(999, "Oxford str"),
-                                city = "That city",
-                                state = "Bay of Plenty",
-                                country = "Australia",
-                                postCode = "32336"
-                            ),
-                            email = "william.henry.harrison@example-pet-store.com",
-                            phone = "123-456-7890",
-                            picture = com.paint.randompeoplek.service.model.Picture(
-                                large = "https://randomuser.me/api/portraits/men/1.jpg",
-                                medium = "https://randomuser.me/api/portraits/med/men/1.jpg",
-                                thumbnail = "https://randomuser.me/api/portraits/thumb/men/1.jpg"
-                            )
-                        )
-                    )
-                )
-            )
-        }
-    }
-
-
-    @Before
-    fun setup() {
-        hiltRule.inject()
-    }
-
-
-//    @BindValue
-//    @JvmField // JvmField is necessary for Hilt to correctly process the @BindValue field
-//    val mockUseCase: RandomPeopleListUseCase = mockk()
-
-//    private fun getFakeUserResponse(count: Int = 10): UserResponse {
-//        val users = List(count) { i ->
-//            User(
-//                id = "id$i",
-//                name = Name(shortName = "First$i", fullName = "Last$i"),
-//                location = "Location $i",
-//                email = "email$i@example.com",
-//                phone = "123-456-789$i",
-//                picture = Picture(
-//                    medium = "https://randomuser.me/api/portraits/med/men/${i + 1}.jpg",
-//                    thumbnail = "https://randomuser.me/api/portraits/thumb/men/${i + 1}.jpg"
-//                )
-//            )
-//        }
-//        return UserResponse(users = users, networkError = null)
-//    }
-
     @Test
     fun testSuccessScenario() = runBlocking<Unit> {
-        // Configure the mock to return a successful response with fake data
-        //coEvery { mockUseCase.getUserList(any()) } returns getFakeUserResponse(10)
-
         hiltRule.inject()
-
 
         composeRule.onNodeWithText("Random People").assertIsDisplayed()
 
@@ -183,75 +75,18 @@ class RandomPeopleMainActivityTest {
     }
 
     @Test
-    fun testSearchScenarioSuccess() = runBlocking<Unit> {
-        //                return Response.success(
-//                    200,
-//                    com.paint.randompeoplek.service.model.UserResponse(
-//                        listOf(
-//                            com.paint.randompeoplek.service.model.User(
-//                                id = Id("test_id"),
-//                                name = com.paint.randompeoplek.service.model.Name("Mr", "Ryan", "Wilson"),
-//                                location = com.paint.randompeoplek.service.model.Location(
-//                                    street = com.paint.randompeoplek.service.model.Street(740, "Lambie Drive"),
-//                                    city = "Invercargill",
-//                                    state = "Bay of Plenty",
-//                                    country = "New Zealand",
-//                                    postCode = "32336"
-//                                ),
-//                                email = "william.henry.harrison@example-pet-store.com",
-//                                phone = "123-456-7890",
-//                                picture = com.paint.randompeoplek.service.model.Picture(
-//                                    large = "https://randomuser.me/api/portraits/men/1.jpg",
-//                                    medium = "https://randomuser.me/api/portraits/med/men/1.jpg",
-//                                    thumbnail = "https://randomuser.me/api/portraits/thumb/men/1.jpg"
-//                                )
-//                            )
-//                        )
-//                    )
-//                )
-
-//        coEvery { randomPeopleService.getUserList(any()) } answers {
-//            Response.success(
-//                    200,
-//                    com.paint.randompeoplek.service.model.UserResponse(
-//                        listOf(
-//                            com.paint.randompeoplek.service.model.User(
-//                                id = Id("test_id"),
-//                                name = com.paint.randompeoplek.service.model.Name("Mr", "Ryan", "Wilson"),
-//                                location = com.paint.randompeoplek.service.model.Location(
-//                                    street = com.paint.randompeoplek.service.model.Street(740, "Lambie Drive"),
-//                                    city = "Invercargill",
-//                                    state = "Bay of Plenty",
-//                                    country = "New Zealand",
-//                                    postCode = "32336"
-//                                ),
-//                                email = "william.henry.harrison@example-pet-store.com",
-//                                phone = "123-456-7890",
-//                                picture = com.paint.randompeoplek.service.model.Picture(
-//                                    large = "https://randomuser.me/api/portraits/men/1.jpg",
-//                                    medium = "https://randomuser.me/api/portraits/med/men/1.jpg",
-//                                    thumbnail = "https://randomuser.me/api/portraits/thumb/men/1.jpg"
-//                                )
-//                            )
-//                        )
-//                    )
-//                )
-//        }
-
-//        hiltRule.inject()
+    fun testSearchBar() {
+        hiltRule.inject()
 
         composeRule.onNodeWithText("Random People").assertIsDisplayed()
 
-//        composeRule.onNodeWithTag(TEST_TAG_RANDOM_PEOPLE_LIST_USERS).assertIsDisplayed()
-//        composeRule.onNodeWithTag(TEST_TAG_RANDOM_PEOPLE_LIST_USERS)
-//            .performScrollToNode(
-//                hasTestTag(TEST_TAG_RANDOM_PEOPLE_LIST_LAST_USER)
-//            )
-
-        delay(12000)
-
+        composeRule.onNodeWithTag(TEST_TAG_RANDOM_PEOPLE_LIST_USERS).assertIsDisplayed()
+        composeRule.onNodeWithTag(TEST_TAG_RANDOM_PEOPLE_LIST_USERS)
+            .performScrollToNode(
+                hasTestTag(TEST_TAG_RANDOM_PEOPLE_LIST_LAST_USER)
+            )
         composeRule.onAllNodesWithTag(TEST_TAG_RANDOM_PEOPLE_LIST_USER)
-            .assertCountEquals(1)
+            .assertCountEquals(9)
         composeRule.onAllNodesWithTag(TEST_TAG_RANDOM_PEOPLE_LIST_LAST_USER)
             .assertCountEquals(1)
 
@@ -259,10 +94,6 @@ class RandomPeopleMainActivityTest {
         composeRule.onNodeWithTag(TEST_TAG_SEARCH_CLOSE_ICON).performClick()
         composeRule.onNodeWithTag(TEST_TAG_SEARCH_ICON).performClick()
 
-//        val nodeInteraction = composeRule.onNodeWithTag(TEST_TAG_RANDOM_PEOPLE_LIST_USERS).onChildAt(1)
-//        val node = nodeInteraction.fetchSemanticsNode()
-//        //node.config.getOrNull(SemanticsProperties.Text)
-//
-//        composeRule.onNodeWithTag(TEST_TAG_SEARCH_TEXT_FILED).performTextReplacement("Test")
+        composeRule.onNodeWithTag(TEST_TAG_SEARCH_TEXT_FILED).performTextReplacement("Test")
     }
 }
